dda6c972ef4372425e7c944169e594c40ff3b2da
CiobanuMarian <49883613+CiobanuMarian@users.noreply.github.com>
2020-11-07 02:34:05 +0200

    Added settings page(not functional), modified main UI

--- app/src/main/java/com/msolutions/flo_app/MainActivity.java
+++ app/src/main/java/com/msolutions/flo_app/MainActivity.java
@@ -1,10 +1,10 @@
 package com.msolutions.flo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.view.View;
@@ -13,20 +13,21 @@ import android.widget.Chronometer;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private Button btnPlay;
     private Button btnPause;
+    private TextView txtLeave;
+    private ImageView imgLeave;
     private ImageView imgView;
+    private ImageView imgSettings;
     private Chronometer chronometer;
+    private TextView txtSettings;
     private Handler handler;
     private long tMilliSec, tStart, tBuff, tUpdate = 0L;
     private int sec, min, milliSec;
@@ -35,16 +36,46 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btnPlay = findViewById(R.id.btnPlay);
         btnPause = findViewById(R.id.btnPause);
         chronometer = findViewById(R.id.chronometer);
-//        chronometer.setVisibility(View.GONE);
+//        chronometer.setVisibility(View.GONE); //disable the timer
         imgView = findViewById(R.id.imgColor);
         btnPause.setVisibility(View.GONE);
         handler = new Handler();
+
+        imgSettings = findViewById(R.id.imgSettings);
+        imgSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this
+                        , SettingsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+        txtSettings = findViewById(R.id.txtSettings);
+        txtSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this
+                        , SettingsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+        imgLeave = findViewById(R.id.imgLeave);
+        imgLeave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
 
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,16 +103,28 @@ public class MainActivity extends AppCompatActivity {
         btnPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(started){
+                if (started) {
                     started = false;
                     tBuff += tMilliSec;
                     handler.removeCallbacks(runnable);
                     chronometer.stop();
                     btnPause.setVisibility(View.GONE);
-                    btnPlay.setVisibility(View.VISIBLE);;
+                    btnPlay.setVisibility(View.VISIBLE);
+                    ;
                 }
             }
         });
+
+        txtLeave = findViewById(R.id.txtLeave);
+        txtLeave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
     }
 
     public Runnable runnable = new Runnable() {
@@ -95,8 +138,8 @@ public class MainActivity extends AppCompatActivity {
             milliSec = (int) (tUpdate % 100);
             chronometer.setText(String.format("%02d", min) + ":" + String.format("%02d", sec) + ":" + String.format("%02d", milliSec));
             handler.postDelayed(this, 60);
-            if(Settings.getInstance().getTimeChange() == 0) {
-                if(sec/Settings.DEFAULT_TIME >= count){
+            if (Settings.getInstance().getTimeChange() == 0) {
+                if (sec / Settings.DEFAULT_TIME >= count) {
                     imgView.setColorFilter(generateColor());
                     count++;
                 }
@@ -107,15 +150,20 @@ public class MainActivity extends AppCompatActivity {
 
 
     //Hard-codded available colors, TBD which the user should have
-    public int generateColor(){
-        final int random = new Random().nextInt(5);
+    public int generateColor() {
+        final int random = new Random().nextInt(50);
         List<Integer> availableColors = new ArrayList<>();
-        availableColors.add(Color.BLUE);
-        availableColors.add(Color.MAGENTA);
-        availableColors.add(Color.RED);
-        availableColors.add(Color.GREEN);
-        availableColors.add(Color.YELLOW);
-        return availableColors.get(random);
+        availableColors.add(0xFFFF5722); // orange
+        availableColors.add(0xFF03A9F4); // blue
+        availableColors.add(0xFF673AB7); //purple
+        availableColors.add(0xFFE91E63); //pink
+        availableColors.add(0xFF4CAF50); //green
+        return availableColors.get(random % 5);
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
---
+++ app/src/main/java/com/msolutions/flo_app/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.msolutions.flo_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+    }
+}
